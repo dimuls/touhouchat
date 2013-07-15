4bcc6c35f596f331886e145e454e7804cfef085f
Вадим Чернов <dimuls@yandex.ru>
2013-07-15 10:03:47 +0400

    Fixes

--- app/app.js
+++ app/app.js
@@ -20,8 +20,8 @@ var escapeHtml = require('escape-html')
 
 io.configure('production', function() {
   io.enable('browser client minification');  // send minified client
-  io.enable('browser client etag');          // apply etag caching logic based on version number
-  io.enable('browser client gzip');          // gzip the file
+//  io.enable('browser client etag');          // apply etag caching logic based on version number
+//  io.enable('browser client gzip');          // gzip the file
   io.set('log level', 1);                    // reduce logging
   io.set('transports', [                     // enable all transports (optional if you want flashsocket)
       'websocket'
